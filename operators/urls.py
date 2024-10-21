919540b6ce7d104178277463c2d2997a56818811
Siddhartha Dutta <2897074d@student.gla.ac.uk>
2024-10-21 22:24:42 +0100

    Operator Functionality

--- operators/urls.py
+++ operators/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from operators.views import DashboardView, LoginView
+from operators.views import DashboardView, LoginView, VehicleDetailView, VehicleListView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='operator_login'),
     path('', DashboardView.as_view(), name='operator_dashboard'),
+    path('vehicles/', VehicleListView.as_view(), name='vehicle_list'),
+    path('vehicles/<int:pk>/', VehicleDetailView.as_view(), name='vehicle_detail'),
 ]
