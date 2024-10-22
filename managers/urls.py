f2f6b128e542a61f6a8fd88081449c575425e139
Siddhartha Pratim Dutta <2897074d@student.gla.ac.uk>
2024-10-22 01:11:52 +0100

    Vehicle Count

--- managers/urls.py
+++ managers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from managers.views import DashboardView, LoginView, VehicleUsageView
+from managers.views import DashboardView, LoginView, VehicleCountView, VehicleUsageView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='manager_login'),
     path('', DashboardView.as_view(), name='manager_dashboard'),
+    path('vehicle-count/', VehicleCountView.as_view(), name='vehicle_count'),
     path('vehicle-usage/', VehicleUsageView.as_view(), name='vehicle_usage'),
 ]
--- managers/views.py
+++ managers/views.py
@@ -1,8 +1,10 @@
-from django.db.models import Count
+from collections import defaultdict
+
+from django.db.models import Count, Q
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
-from core.models import Trip
+from core.models import Trip, Vehicle
 from managers.mixins import DateFilterMixin, LoginRequiredMixin
 from users.models import CustomUser
 from users.views import BaseUserLoginView
@@ -15,6 +17,38 @@ class LoginView(BaseUserLoginView):
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'managers/dashboard.html'
+
+
+class VehicleCountView(LoginRequiredMixin, TemplateView):
+    template_name = 'managers/vehicle_count.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        query = (
+            Vehicle.objects.filter(~Q(status=Vehicle.Status.IN_USE))
+            .select_related('location')
+            .values('location__name', 'status')
+            .annotate(count=Count('code'))
+        )
+
+        location_data = defaultdict(
+            lambda: {Vehicle.Status.AVAILABLE: 0, Vehicle.Status.DEFECTIVE: 0}
+        )
+        for item in query:
+            location_data[item['location__name']][item['status']] = item['count']
+        locations = []
+        available_counts = []
+        defective_counts = []
+        for location_name, counts in location_data.items():
+            locations.append(location_name)
+            available_counts.append(counts['available'])
+            defective_counts.append(counts['defective'])
+
+        context['locations'] = locations
+        context['available_counts'] = available_counts
+        context['defective_counts'] = defective_counts
+        return context
 
 
 class VehicleUsageView(LoginRequiredMixin, DateFilterMixin, TemplateView):
