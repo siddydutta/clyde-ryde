7ca52cc63ff13df689a9f117cae4323c644715e6
Siddhartha Dutta <2897074d@student.gla.ac.uk>
2024-11-04 18:12:25 +0000

    Bugfixes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py migrate
 python manage.py add_users
 python manage.py add_locations
 python manage.py add_vehicles --number 15
-python manage.py add_trips --start_date 2024-10-01 --number 30
+python manage.py add_trips --start_date 2024-10-01 --number 50
 
 # Start server
 python manage.py runserver 0.0.0.0:8000
